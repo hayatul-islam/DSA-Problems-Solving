b10e6019032dfba167a6ad679bf84a44bb3e8fd2
hayatul-islam <hayatulemon340@gmail.com>
2025-01-15 12:37:38 +0600

    bubble sort another way implement

--- DSA-Explorer/bubbleSort.js
+++ DSA-Explorer/bubbleSort.js
@@ -2,7 +2,7 @@
 
 function bubbleSort(arr) {
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - 1; j++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
         arr[j] = arr[j + 1];
@@ -13,4 +13,16 @@ function bubbleSort(arr) {
   return arr;
 }
 
-console.log(bubbleSort([2, 55, 44, 77, -5]));
+function bubbleSort2(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(bubbleSort([2, 44, 77, -5]));
+console.log(bubbleSort2([2, 44, 77, -5]));
