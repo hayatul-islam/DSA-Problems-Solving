824cd622a5005960645921d555b312de58ae0c7c
hayatul-islam <hayatulemon340@gmail.com>
2025-01-15 14:14:45 +0600

    code optimized

--- DSA-Explorer/bubbleSort.js
+++ DSA-Explorer/bubbleSort.js
@@ -24,5 +24,21 @@ function bubbleSort2(arr) {
   return arr;
 }
 
+// optimized code
+function bubbleSort3(arr) {
+  let isSwap;
+  for (let i = 0; i < arr.length; i++) {
+    isSwap = false;
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        isSwap = true;
+      }
+    }
+    if (!isSwap) break;
+  }
+  return arr;
+}
+
 console.log(bubbleSort([2, 44, 77, -5]));
-console.log(bubbleSort2([2, 44, 77, -5]));
+console.log(bubbleSort3([2, 44, 77, -5]));
