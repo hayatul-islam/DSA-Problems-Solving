8e0bd84702fcd8342bc52986daba564f2a6c2a49
hayatul-islam <hayatulemon340@gmail.com>
2025-01-21 12:45:52 +0600

    linked list pop method implement

--- DSA-Explorer/data-structures/linked-list/singlyLinkedList.js
+++ DSA-Explorer/data-structures/linked-list/singlyLinkedList.js
@@ -24,13 +24,46 @@ class SinglyLinkedList {
     }
     this.length++;
   }
+
+  pop() {
+    if (!this.head) {
+      return null;
+    }
+    if (this.length === 1) {
+      const removeNode = this.head;
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+      return removeNode;
+    }
+
+    let currentNode = this.head;
+    let lastNode = this.tail;
+    let newLastNode;
+
+    while (currentNode) {
+      if (currentNode.next === this.tail) {
+        newLastNode = currentNode;
+        break;
+      }
+      currentNode = currentNode.next;
+    }
+
+    newLastNode.next = null;
+    this.tail = newLastNode;
+    this.length--;
+
+    return lastNode;
+  }
 }
 
 const list = new SinglyLinkedList();
 
-console.log(list.isEmpty());
 list.push(4);
 list.push(6);
 list.push(10);
+list.push(14);
+list.pop();
 
+// console.log(list.isEmpty());
 console.log(list);
