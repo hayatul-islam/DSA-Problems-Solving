b5dd74f8f1295eed0aab902ebd9aac50a4fdf26f
hayatul-islam <hayatulemon340@gmail.com>
2025-01-29 22:51:39 +0600

    Form The Minimum problem solve

--- Codewars/7-KYU/minValue.js
+++ Codewars/7-KYU/minValue.js
@@ -7,6 +7,32 @@
 // [5, 7, 5, 9, 7] ==> 579
 // [1, 9, 3, 1, 7, 4, 6, 6, 7]  ==> 134679
 
-function minValue(values) {}
+function minValue(values) {
+  let hash = {};
+  let arr = [];
+  let result = "";
 
-console.log(minValue([1, 3, 1]));
+  for (let i = 0; i < values.length; i++) {
+    if (!hash[values[i]]) {
+      hash[values[i]] = values[i];
+      arr.push(values[i]);
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    result += arr[i];
+  }
+
+  return Number(result);
+}
+
+// console.log(minValue([4, 1, 3, 1]));
+console.log(minValue([5, 6, 9, 9, 7, 6, 4]));
