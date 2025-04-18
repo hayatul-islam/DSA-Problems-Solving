90a7358677741b9fbf0ad743757601b24f5c6c24
hayatul-islam <hayatulemon340@gmail.com>
2025-04-18 22:56:23 +0600

    smallest pair problem solve

---
+++ I_Smallest_Pair.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <limits.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        int n;
+        scanf("%d", &n);
+
+        int arr[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &arr[i]);
+        }
+
+        int lowestSum = INT_MAX;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                int total = arr[i] + arr[j] + j + 1 - (i + 1);
+                if (lowestSum > total)
+                {
+                    lowestSum = total;
+                }
+            }
+        }
+        printf("%d\n", lowestSum);
+    }
+
+    return 0;
+}
