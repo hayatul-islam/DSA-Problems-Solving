f803157c5632d9d71219e94035bd9c20b726045e
hayatul-islam <hayatulemon340@gmail.com>
2025-04-19 20:10:59 +0600

    replace min max problem solve

---
+++ Codeforces/c/M_Replace_MinMax.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int min = arr[0];
+    int max = arr[0];
+    int minIdx = 0;
+    int maxIdx = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (min > arr[i])
+        {
+            min = arr[i];
+            minIdx = i;
+        }
+
+        if (arr[i] > max)
+        {
+            max = arr[i];
+            maxIdx = i;
+        }
+    }
+
+    arr[minIdx] = max;
+    arr[maxIdx] = min;
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
