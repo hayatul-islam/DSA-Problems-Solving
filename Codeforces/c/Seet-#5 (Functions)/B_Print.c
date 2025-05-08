0466a52f214b8203b9d286dec2ff34be06b08024
hayatul-islam <hayatulemon340@gmail.com>
2025-05-08 12:10:06 +0600

    print function

---
+++ Codeforces/c/Seet-#5 (Functions)/B_Print.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int printN(n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        if (i == n)
+        {
+            printf("%d", i);
+        }
+        else
+        {
+
+            printf("%d ", i);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    printN(n);
+
+    return 0;
+}
