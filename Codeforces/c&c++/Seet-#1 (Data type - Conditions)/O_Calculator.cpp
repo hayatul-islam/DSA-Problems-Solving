b8e8322e2efd5082de8d97c893f7e25fdf0ee8ed
hayatul-islam <hayatulemon340@gmail.com>
2025-09-15 21:25:43 +0600

    calculator

---
+++ Codeforces/c&c++/Seet-#1 (Data type - Conditions)/O_Calculator.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    char c;
+    cin >> a >> c >> b;
+
+    if (c == '+')
+        cout << a + b;
+    else if (c == '-')
+        cout << a - b;
+    else if (c == '*')
+        cout << a * b;
+    else if (c == '/')
+        cout << a / b;
+
+    return 0;
+}
