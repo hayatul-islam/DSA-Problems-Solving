e7f05d941bafe6f985f4869ffea730c72e4e3822
hayatul-islam <hayatulemon340@gmail.com>
2025-09-15 21:50:23 +0600

    first digit

---
+++ Codeforces/c&c++/Seet-#1 (Data type - Conditions)/P_First_digit.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    while (n >= 10)
+        n /= 10;
+
+    if (n % 2 == 0)
+        cout << "EVEN";
+    else
+        cout << "ODD";
+
+    return 0;
+}
