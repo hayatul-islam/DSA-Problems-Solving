b12849934d4c0a92cfcd1c092269d6b1920928f8
hayatul-islam <hayatulemon340@gmail.com>
2025-09-15 19:29:58 +0600

    basic data type

---
+++ Codeforces/c&c++/Seet-#1 (Data type - Conditions)/B_Basic_Data_Types.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    long long int lln;
+    char c;
+    double d;
+    float f;
+
+    cin >> n >> lln >> c >> d >> f;
+    cout << n << endl
+         << lln << endl
+         << c << endl
+         << d << endl
+         << f << endl;
+    return 0;
+}
