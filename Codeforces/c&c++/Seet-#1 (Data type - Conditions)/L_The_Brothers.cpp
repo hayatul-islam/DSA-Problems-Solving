9381275ee3b6b374e64245eface0377af59e85ff
hayatul-islam <hayatulemon340@gmail.com>
2025-09-15 21:14:20 +0600

    the brothers

---
+++ Codeforces/c&c++/Seet-#1 (Data type - Conditions)/L_The_Brothers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string f1, f2, s1, s2;
+    cin >> f1 >> f2 >> s1 >> s2;
+
+    if (f2 == s2)
+        cout << "ARE Brothers";
+    else
+        cout << "NOT";
+    return 0;
+}
