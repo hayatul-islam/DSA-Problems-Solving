95ae53a911009087222ecbb108d631b51395c3aa
hayatul-islam <hayatulemon340@gmail.com>
2025-09-15 19:59:57 +0600

    area of circle

---
+++ Codeforces/c&c++/Seet-#1 (Data type - Conditions)/E_Area_of_a_Circle.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    double d;
+    cin >> d;
+    double result = pow(d, 2) * 3.141592653;
+    cout << fixed << setprecision(9) << result;
+    return 0;
+}
