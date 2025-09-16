4031e07b51ad7e5066bb5d47c7583a69fb59114d
hayatul-islam <hayatulemon340@gmail.com>
2025-09-16 11:46:18 +0600

    the last 2 digits

---
+++ Codeforces/c&c++/Seet-#1 (Data type - Conditions)/Y_The_last_2_digits.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    long long int result = 1;
+    result = (result * a) % 100;
+    result = (result * b) % 100;
+    result = (result * c) % 100;
+    result = (result * d) % 100;
+
+    if (result < 10)
+        cout << "0" << result;
+    else
+        cout << result;
+
+    return 0;
+}
